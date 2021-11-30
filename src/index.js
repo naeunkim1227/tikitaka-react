eb217b03ef254f05b56f999ab1121ea8e8634809
naeunkim1227 <kne0140@daum.net>
2021-11-30 19:04:37 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+import {App} from './App.js';
+
+
+//document
+//    .getElementById('root')
+//    .appendChild(App());
+
+ReactDOM.render(App(), document.getElementById('root'));
+
+//root를 다음의 내용으로 바꾼다.
