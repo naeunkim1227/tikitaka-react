5d71cea6f0b18a7c4e05e8db4247f455dddc4878
naeunkim1227 <kne0140@daum.net>
2021-12-01 20:59:04 +0900

    reset

--- tikitaka/src/index.js
+++ tikitaka/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app/App';
+import "./i18n";
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter basename="/demo/purple-react-free/template/demo_1/preview">
+    <App />
+  </BrowserRouter>
+, document.getElementById('root'));
 
+serviceWorker.unregister();
