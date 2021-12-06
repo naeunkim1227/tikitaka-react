22fa059c3ee08ad1c09bb2664523f9d1a6bd979b
jung-jinwoo <wlsdn960319@gmail.com>
2021-12-06 18:33:06 +0900

    jinwoo / create / Profile

---
+++ src/pages/UpdateProfile.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+import React from 'react';
+
+const UpdateProfile = () => {
+    return (
+        <div>
+            <h1>Profile 변경화면</h1>
+        </div>
+    );
+};
+
+export default UpdateProfile;
