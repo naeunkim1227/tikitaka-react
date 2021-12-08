4dbb4966658ee69ad0587a016731bd71905a3381
jung-jinwoo <wlsdn960319@gmail.com>
2021-12-08 21:18:35 +0900

    jinwoo / [update] /ChatRoom

--- src/chat/ChatRoom.js
+++ src/chat/ChatRoom.js
@@ -1,31 +1,99 @@
 /* eslint-disable */ 
 
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './components.css';
 import './style.css';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Button from '@mui/material/Button';
+// import SendIcon from '@mui/icons-material/Send';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TextField from '@mui/material/TextField';
+import { green } from '@mui/material/colors';
+import Icon from '@mui/material/Icon';
+import axios from 'axios';
 
 const ChatRoom = () => {
+    const [message, setMessage] = useState([]);
+
+    const messageHandle = (e) => {
+      setMessage(e.target.value);
+    }
+
+    const sendMessage = async (e) => {
+      e.preventDefault();
+      
+      let data= {
+        userNo: '2',
+        chatNo: '5',
+        type: "TEXT",
+        contents: message,
+        readCount: 3
+      };
+
+      await axios.post('/TT/send', JSON.stringify(data), {headers:{"Content-Type":"application/json"}})
+      .then((response) => {
+        console.log("response:"+response);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+
+    }
 
     return (
-        <div class="col-12 col-sm-6 col-lg-4">
-        <div class="card chat-box card-success" id="mychatbox2">
-          <div class="card-header">
-            <h4><i class="fas fa-circle text-success mr-2" title="Online" data-toggle="tooltip"></i> Chat with Ryan</h4>
-          </div>
-          <div class="card-body chat-content">
-          </div>
-          <div class="card-footer chat-form">
-            <form id="chat-form2">
-              <input type="text" class="form-control" placeholder="Type a message"/>
-              <button class="btn btn-primary">
-                <i class="far fa-paper-plane"></i>
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
+      <Card sx={{ minWidth: 275 }}>
+      <CardContent style={{borderBottom: "2px solid gray"}}>
+        <h1>채팅방 이름, 검색창</h1>
+      </CardContent>
+      <CardContent sx={{ minWidth: 600 , minHeight:450}}>
 
-    );
+        {/* <Table colspan="5" sx={{ minWidth: 220 , minHeight:150}} aria-label="simple table">
+          <TableBody rowsPerPageOptions={[5]} align="center">
+            <tr>
+              <td colspan="1">hi</td>
+              <td colspan="4"></td>
+            </tr>
+            <tr>
+              <td colspan="1">bye</td>
+              <td colspan="4" rowspan="2">
+                bye
+              </td>
+            </tr>
+          </TableBody>
+        </Table> */}
+      </CardContent>
+      <CardContent style={{ borderTop: "2px solid gray", margin: 10, padding: 10, alignItems: 'start'}}>
+      <form onSubmit={sendMessage}>
+        <Button>
+            <Icon sx={{ color: green[500], 
+                  width: 50, 
+                  height: 50, 
+                  fontSize: 30, 
+                  border: '2px solid gray',
+                  borderRadius: '5px'}}>+</Icon>
+        </Button>
+      
+        <TextField
+          inputMode
+          hiddenLabel
+          id="textWindow"
+          placeholder='메시지를 입력하시오.'
+          variant="outlined"
+          style={{align:"center", left:200 , marginRight: "10px" , minWidth: 750}}
+          type='text'
+          name="message"
+          onChange={messageHandle}
+        />
+        
+        <Button type='submit' variant="contained" style={{position: 'absolute', right:100, marginRight: "10px"}} size="large">
+          Send
+        </Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default ChatRoom;
