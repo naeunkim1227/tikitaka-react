3c8371415fb5fc3a185816ac0fae22ab3fbbc966
jung-jinwoo <wlsdn960319@gmail.com>
2021-12-11 21:13:50 +0900

    jinwoo / [update] / joinform

--- src/components/authentication/register/RegisterForm.js
+++ src/components/authentication/register/RegisterForm.js
@@ -1,7 +1,7 @@
 /* eslint-disable */ 
 
 import * as Yup from 'yup';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Icon } from '@iconify/react';
 import { useFormik, Form, FormikProvider } from 'formik';
 import eyeFill from '@iconify/icons-eva/eye-fill';
@@ -21,7 +21,7 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 export default function RegisterForm() {
   const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
-  const [state, setState] = useState('CP');
+  const [state, setState ]= useState('');
 
   const RegisterSchema = Yup.object().shape({
     userNo: Yup.string()
@@ -36,15 +36,17 @@ export default function RegisterForm() {
 
   const formik = useFormik({
     initialValues: {
-      role: state,
+      role: '', 
       userNo: '',
       name: '',
       password: '',
       email: '',
-      phone: ''
+      phone: '',
+      proName: ''
     },
     validationSchema: RegisterSchema,
     onSubmit: async (data) => {
+      console.log(data.role);
       const response = await axios.post('/TT/join', data)
                       .then((response) => {
                         console.log(response);
@@ -52,17 +54,20 @@ export default function RegisterForm() {
                           console.log("들어오니? 쥐리제");
                           throw  `${response.status} ${response.statusText}`;
                         }
-                        navigate('/login', { replace: true });
+                        //navigate('/login', { replace: true });
                       })
                       .catch((error) => {
                         console.log(error);
                       })
+                      
     }
 
   });
 
   const chooseRole = (e) => {
-    setState(e.target.value.toString());
+    
+    setState(e.target.value);
+    formik.initialValues.role = state;
   };
  
   const { errors, touched,handleSubmit, isSubmitting, getFieldProps } = formik;
@@ -75,10 +80,13 @@ export default function RegisterForm() {
             onSubmit={handleSubmit}>
         <Stack spacing={3}>
           <RadioGroup defaultValue="CP" row aria-label="role" name="radio-group" >
-          <FormControlLabel type="text" value="CP"  control={<Radio />} label="사원" checked={state === "CP"} onChange={chooseRole}/>
-          <FormControlLabel type="text" value="CS"  control={<Radio />} label="고객" checked={state === "CS"} onChange={chooseRole}/>
+          <FormControlLabel name="role" type="text" value="CP"  control={<Radio />} label="사원" checked={state === "CP"}  onChange={chooseRole}/>
+          <FormControlLabel name="role" type="text" value="CS"  control={<Radio />} label="고객" checked={state === "CS"}  onClick={chooseRole}/>
           </RadioGroup>
-          <TextField
+          {
+            state == "CP" ? 
+            <div>
+            <TextField
             fullWidth
             autoComplete="userNo"
             type="text"
@@ -86,9 +94,9 @@ export default function RegisterForm() {
             value={formik.userNo}
             {...getFieldProps('userNo')}
             error={Boolean(touched.userNo && errors.userNo)}
-            helperText={touched.userNo && errors.userNo}
-          />
-          <TextField
+            helperText={touched.userNo && errors.userNo}/> 
+            
+            <TextField
             fullWidth
             autoComplete="name"
             type="test"
@@ -138,7 +146,86 @@ export default function RegisterForm() {
             error={Boolean(touched.phone && errors.phone)}
             helperText={touched.phone && errors.phone}
           />
+          </div>
 
+          : 
+          <div>
+          <TextField
+            fullWidth
+            autoComplete="userNo"
+            type="text"
+            label="거래처번호"
+            value={formik.userNo}
+            {...getFieldProps('userNo')}
+            error={Boolean(touched.userNo && errors.userNo)}
+            helperText={touched.userNo && errors.userNo}
+          />
+          <TextField
+            fullWidth
+            autoComplete="password"
+            type={showPassword ? 'text' : 'password'}
+            label="비밀번호"
+            value={formik.password}
+            {...getFieldProps('password')}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton edge="end" onClick={() => setShowPassword((prev) => !prev)}>
+                    <Icon icon={showPassword ? eyeFill : eyeOffFill} />
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+            error={Boolean(touched.password && errors.password)}
+            helperText={touched.password && errors.password}
+          />
+
+          <TextField
+            fullWidth
+            autoComplete="name"
+            type="test"
+            label="거래처명"
+            value={formik.name}
+            {...getFieldProps('name')}
+            error={Boolean(touched.name && errors.name)}
+            helperText={touched.name && errors.name}
+          />
+          <TextField
+            fullWidth
+            autoComplete="email"
+            type="email"
+            label="이메일"
+            value={formik.email}
+            {...getFieldProps('email')}
+            error={Boolean(touched.email && errors.email)}
+            helperText={touched.email && errors.email}
+          />
+          <TextField
+            fullWidth
+            autoComplete="phone"
+            type="text"
+            label="전화번호"
+            value={formik.phone}
+            {...getFieldProps('phone')}
+            error={Boolean(touched.phone && errors.phone)}
+            helperText={touched.phone && errors.phone}
+          />
+
+            <TextField
+            fullWidth
+            autoComplete="proName"
+            type="test"
+            label="제품명"
+            value={formik.name}
+            {...getFieldProps('proName')}
+            error={Boolean(touched.name && errors.name)}
+            helperText={touched.name && errors.name}
+          />
+
+          </div>
+          }
+          
+          
           <LoadingButton
             fullWidth
             size="large"
